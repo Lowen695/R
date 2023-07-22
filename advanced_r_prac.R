2fdc58ef5f2da0b70806e125e071337cdfec869e
Fahui Liu <fahuiliu@Fahuis-MacBook-Pro.local>
2023-07-22 18:06:01 +0200

    advR update

--- advanced_r_prac.R
+++ advanced_r_prac.R
@@ -1,3 +1,16 @@
 library(tidyverse)
 
-getwd()
+df <- data.frame(a = 1:5, b = c('a','b','c','d','e'), stringsAsFactors = TRUE)
+type.convert(df)
+str(df)
+
+df2 <- data.frame(x = 1:3, y = I(list(1:2,1:3,1:4)))
+
+dfm <- data.frame(x = 1:3, y = I(matrix(1:9, nrow = 3)))
+
+df[3,'b']
+
+mta <- matrix(1:9, nrow = 3)
+colnames(mta) <- c('A','B','C')
+mta[1:2,]
+mta[]
