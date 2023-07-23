681ddb38ad0bd77171fe19bedb05a1cd90f5ed84
Fahui Liu <fahuiliu@Fahuis-MacBook-Pro.local>
2023-07-23 11:50:39 +0200

    new

--- advanced_r_prac.R
+++ advanced_r_prac.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-df <- data.frame(a = 1:5, b = c('a','b','c','d','e'), stringsAsFactors = TRUE)
+df <- data.frame(aiu = 1:5, aboap = c('auyt','b','cop','d','e'), stringsAsFactors = TRUE)
 type.convert(df)
 str(df)
 
@@ -16,3 +16,26 @@ mta[1:2,]
 mta[9]
 
 vals <- outer(1:5, 1:5, FUN = "paste", sep = ",")
+
+df2 <- outer(LETTERS[1:6],letters[1:6],FUN = 'paste', sep=':')
+
+df$a
+
+x <- 1:4
+str(x[[5]])
+
+df[] <- lapply(df, as.integer)
+
+x <- c("m", "f", "u", "f", "f", "m", "m")
+lookup <- c(m = "Male", f = "Female", u = NA)
+unname(lookup[x])
+
+grades <- c(1, 2, 2, 3, 1)
+info <- data.frame(
+  grade = 3:1,
+  desc = c("Excellent", "Good", "Poor"), fail = c(F, F, T)
+)
+
+id <- match(grades,info$grade)
+info[id,]
+
